b8e9bf00f183deb172c9f9c0d04e940943eddd67
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-04-07 19:16:56 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>()
+            {
+                "DOOM", "Farcry", "Team Fortres", "Spore", "Doki Doki Literature Club", "Bendy and the Ink Machine", "Armello","Oneshot"
+            };
+            games = games.OrderBy(x => x.Length).ToList();
+            foreach (string game in games) 
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
